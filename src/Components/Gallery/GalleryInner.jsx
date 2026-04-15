9a73711a3044fa226df7833bbac9e23e80d50802
OthmanGhTheArk <othman.ghandour@thearknetworks.com>
2026-04-15 12:26:44 +0300

    Update GalleryInner.jsx

--- src/Components/Gallery/GalleryInner.jsx
+++ src/Components/Gallery/GalleryInner.jsx
@@ -1,148 +1,90 @@
-import React, { useState } from 'react';
-import Modal from './Modal';
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import Modal from "./Modal";
+import { Link } from "react-router-dom";
 
 function GalleryInner() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [modalImage, setModalImage] = useState('');
+  const galleryImages = [
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 1.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 2.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 3.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 4.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 5.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 6.png",
+    "/assets/img/gallery/Untitled-1_538 x 720 Image 6 copy.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 1.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 2.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 3.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 4.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 5.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 6.png",
+    "/assets/img/gallery/Untitled-1_670 x 720 Image 7.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 1.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 2.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 3.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 4.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 5.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 6.png",
+    "/assets/img/gallery/Untitled-1_1040 x 720 Image 7.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 1.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 2.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 2 copy.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 4.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 5.png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 6 .png",
+    "/assets/img/gallery/Untitled-1_1352 x 720 Image 6  copy.png",
+  ];
 
-    // Function to open the modal with the selected image
-    const openModal = (imageSrc, event) => {
-        event.preventDefault(); // Prevent default link behavior
-        setModalImage(imageSrc);
-        setIsModalOpen(true);
-    };
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImage, setModalImage] = useState("");
 
-    // Function to close the modal
-    const closeModal = () => {
-        setIsModalOpen(false);
-    };
-    return (
-        <div className="overflow-hidden space" id="gallery-sec">
-            <div className="container-fuild">
-                <div className="title-area mb-30 text-center">
-                    <span className="sub-title">Explore Us</span>
-                    <h2 className="sec-title">A truly exceptional experience</h2>
-                </div>
-                <div className="row gy-4 gallery-row4">
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_1.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_1.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_1.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_2.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_2.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_2.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_3.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_3.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_3.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_4.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_4.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_4.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_5.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_5.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_5.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_6.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_6.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_6.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_7.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_7.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_7.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-auto">
-                        <div className="gallery-box style5">
-                            <div className="gallery-img global-img">
-                                <img src="/assets/img/gallery/gallery_8_8.jpg" alt="gallery" />
-                                <Link
-                                    to="assets/img/gallery/gallery_8_8.jpg"
-                                    className="icon-btn popup-image"
-                                    onClick={(e) => openModal('/assets/img/gallery/gallery_8_8.jpg', e)}
-                                >
-                                    <i className="fal fa-magnifying-glass-plus" />
-                                </Link>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Modal isOpen={isModalOpen} closeModal={closeModal} imageSrc={modalImage} />
+  // Function to open the modal with the selected image
+  const openModal = (imageSrc, event) => {
+    event.preventDefault(); // Prevent default link behavior
+    setModalImage(imageSrc);
+    setIsModalOpen(true);
+  };
+
+  // Function to close the modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <div className="overflow-hidden space" id="gallery-sec">
+      <div className="container-fuild">
+        <div className="title-area mb-30 text-center">
+          <span className="sub-title">
+            From sunlit mornings to shared tables, discover the simple pleasures
+            that define Sardinian living
+          </span>
+          <h2 className="sec-title">A Taste of Sardenia</h2>
+          <p className="sec-text"></p>
         </div>
-
-    )
+        <div className="row gy-4 gallery-row4">
+          {galleryImages.map((imageSrc) => (
+            <div className="col-auto" key={imageSrc}>
+              <div className="gallery-box style5">
+                <div className="gallery-img global-img">
+                  <img src={imageSrc} alt="gallery" />
+                  <Link
+                    to={imageSrc}
+                    className="icon-btn popup-image"
+                    onClick={(e) => openModal(imageSrc, e)}
+                  >
+                    <i className="fal fa-magnifying-glass-plus" />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Modal
+        isOpen={isModalOpen}
+        closeModal={closeModal}
+        imageSrc={modalImage}
+      />
+    </div>
+  );
 }
 
-export default GalleryInner
+export default GalleryInner;
