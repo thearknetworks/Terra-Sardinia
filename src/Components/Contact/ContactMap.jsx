7dd7ff7d988f5490f2aa710a1124616b90febe1a
OthmanGhTheArk <othman.ghandour@thearknetworks.com>
2026-04-16 22:48:48 +0300

    Update ContactMap.jsx

--- src/Components/Contact/ContactMap.jsx
+++ src/Components/Contact/ContactMap.jsx
@@ -6,13 +6,13 @@ function ContactMap() {
       <div className="container-fluid">
         <div className="contact-map style2">
           <iframe
-            src="https://www.google.com/maps?q=39.1508386,9.3990392&z=15&output=embed"
+            src="https://www.google.com/maps?ll=39.1508386,9.3990392&z=15&output=embed"
             title="Torre delle Stelle map location"
             allowFullScreen=""
             loading="lazy"
           />
           <div className="contact-icon">
-            <img src="assets/img/icon/location-dot3.svg" alt="" />
+            <img src="/assets/img/icon/location-dot3.svg" alt="Map marker" />
           </div>
         </div>
       </div>
