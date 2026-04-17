69f1a5322cb8582bb32824aedca131c777222174
OthmanGhTheArk <othman.ghandour@thearknetworks.com>
2026-04-17 07:45:12 +0300

    fix file name

--- src/Components/Resort/VillaVerdeDetailsMain.jsx
+++ src/Components/Resort/VillaVerdeDetailsMain.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import VillaVerdeDetailsInner from "./ResortDetailsMain";
+import VillaVerdeDetailsInner from "./VillaVerdeDetailsInner";
 
 function VillaVerdeDetailsMain() {
   return <VillaVerdeDetailsInner />;
--- src/Pages/ResortDetails.jsx
+++ src/Pages/ResortDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeaderOne from '../Components/Header/HeaderOne'
 import Breadcrumb from '../Components/BreadCrumb/Breadcrumb'
-import VillaVerdeDetailsInner from '../Components/Resort/ResortDetailsMain'
+import VillaVerdeDetailsInner from "../Components/Resort/VillaVerdeDetailsInner";
 import Footer from '../Components/Footer/Footer'
 import ScrollToTop from '../Components/ScrollToTop'
 
